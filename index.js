d47af77cec2efbd2fea4c8c82da785128d676115
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2018-09-07 16:45:54 -0700

    Connect to database

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.get('/', (req, res) => res.send('gamehub-api'));
+
+const db = 'mongodb://localhost/gamehub-api';
+mongoose.connect(db).then(() => console.log(`Connected to ${db}`));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => console.log(`Listening on port ${port}.`));
