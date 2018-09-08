ff1d42ceefbc396c6f44a65a7aa403cf7e68b8d9
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2018-09-07 21:49:32 -0700

    Fix unit test
    Wasn't actually seeing the test fail.

--- test/users.test.js
+++ test/users.test.js
@@ -5,6 +5,7 @@ describe('Users', () => {
     it('should be invalid if name is not provided', async () => {
       const user = new User();
 
+      expect.assertions(1);
       await user.validate()
         .catch((err) => { expect(err.errors.name).toBeDefined(); });
     });
