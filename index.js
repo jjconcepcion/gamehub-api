460b1262995f53acd7d93f314a13f95f94253522
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2018-09-07 17:56:42 -0700

    Add winston module for logging

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
+const winston = require('winston');
 
 const app = express();
 
 app.get('/', (req, res) => res.send('gamehub-api'));
 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'gamehub-api-error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'gamehub-api-combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
 const db = config.get('db');
-mongoose.connect(db).then(() => console.log(`Connected to ${db}`));
+mongoose.connect(db).then(() => logger.info(`Connected to ${db}`));
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => console.log(`Listening on port ${port}.`));
+const server = app.listen(port, () => logger.info(`Listening on port ${port}.`));
 
 module.exports = server;
