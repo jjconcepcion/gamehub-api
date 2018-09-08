629de91374cfe0019ee34e45c185d6b026fb1b86
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2018-09-07 17:11:36 -0700

    Move mongodb uri to defaul config file

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const config = require('config');
 
 const app = express();
 
 app.get('/', (req, res) => res.send('gamehub-api'));
 
-const db = 'mongodb://localhost/gamehub-api';
+const db = config.get('db');
 mongoose.connect(db).then(() => console.log(`Connected to ${db}`));
 
 const port = process.env.PORT || 3000;
