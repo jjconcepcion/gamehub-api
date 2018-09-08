190ccf4b029e9ee63b0e6f799d3b27812310129a
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2018-09-07 23:00:15 -0700

    Add tests requiring email and password be provided

--- test/users.test.js
+++ test/users.test.js
@@ -1,7 +1,14 @@
 const { User } = require('../models/users');
 
 describe('Users', () => {
+  const data = {
+    name: 'aaaa',
+    email: 'aaaa',
+    password: 'aaaa',
+  };
+  
   describe('schema validation', () => {
+
     it('should be invalid if name is not provided', async () => {
       const user = new User();
 
@@ -9,5 +16,26 @@ describe('Users', () => {
       await user.validate()
         .catch((err) => { expect(err.errors.name).toBeDefined(); });
     });
+
+    it('should be invalid if email is not provided', async () => {
+      delete data.email;
+
+      const user = new User(data);
+
+      expect.assertions(1);
+      await user.validate()
+        .catch((err) => { expect(err.errors.email).toBeDefined(); });
+    });
+
+    it('should be invalid if password is not provided', async () => {
+      delete data.password;
+
+      const user = new User(data);
+
+      expect.assertions(1);
+      await user.validate()
+        .catch((err) => { expect(err.errors.password).toBeDefined(); });
+    });
+
   });
 });
