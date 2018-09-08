5cb051f0d247de74164c79f81953a2e6c00da3c9
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2018-09-07 18:20:33 -0700

    Add listen port to config

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ connectToDatabase();
 
 app.get('/', (req, res) => res.send('gamehub-api'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || config.get('listenPort');
 const server = app.listen(port, () => logger.info(`Listening on port ${port}.`));
 
 module.exports = server;
