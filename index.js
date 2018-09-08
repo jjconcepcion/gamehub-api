031dab0639e6e248f2d3c3fae7255c5726ec2cc4
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2018-09-07 18:04:56 -0700

    Move logger to separate module

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
-const winston = require('winston');
+const logger = require('./init/logger');
 
 const app = express();
-
 app.get('/', (req, res) => res.send('gamehub-api'));
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: 'gamehub-api-error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'gamehub-api-combined.log' }),
-  ],
-});
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
 
 const db = config.get('db');
 mongoose.connect(db).then(() => logger.info(`Connected to ${db}`));
