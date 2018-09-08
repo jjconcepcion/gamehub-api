8531757a045465a931697adea9fa8045884e6176
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2018-09-07 21:31:37 -0700

    Refactor unit test
    Async/await syntax seems more readable

--- test/users.test.js
+++ test/users.test.js
@@ -2,11 +2,10 @@ const { User } = require('../models/users');
 
 describe('Users', () => {
   describe('schema validation', () => {
-    it('should be invalid if name is not provided', () => {
+    it('should be invalid if name is not provided', async () => {
       const user = new User();
 
-      expect.assertions(1);
-      return user.validate()
+      await user.validate()
         .catch((err) => { expect(err.errors.name).toBeDefined(); });
     });
   });
