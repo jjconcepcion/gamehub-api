79a979edfcb4f9c3c6fd5d758b52d0c12f9a06d8
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2018-09-07 18:37:18 -0700

    Move home route to routes director

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const config = require('config');
 const logger = require('./init/logger');
 const connectToDatabase = require('./init/db');
+const home = require('./routes/home');
 
 const app = express();
 
 connectToDatabase();
 
-app.get('/', (req, res) => res.send('gamehub-api'));
+app.use('/', home);
 
 const port = process.env.PORT || config.get('listenPort');
 const server = app.listen(port, () => logger.info(`Listening on port ${port}.`));
