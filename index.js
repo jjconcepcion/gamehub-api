869827d4bded5b286e066f49d485ecc2e0dec681
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2018-09-10 14:43:18 -0700

    Add express json middleware function

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const config = require('config');
 const logger = require('./init/logger');
 const connectToDatabase = require('./init/db');
 const home = require('./routes/home');
+const users = require('./routes/users');
 
 const app = express();
 
 connectToDatabase();
 
+app.use(express.json());
+
 app.use('/', home);
+app.use('/api/users', users);
 
 const port = process.env.PORT || config.get('listenPort');
 const server = app.listen(port, () => logger.info(`Listening on port ${port}.`));
